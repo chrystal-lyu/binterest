ec5e89f9922bbb8fa14cee9a898eeced8a684759
Crystal Lu <syyssie@gmail.com>
2017-09-07 18:12:11 +0800

    Added hover effect on photo grids

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -7,35 +7,33 @@ export class Navigation extends React.Component {
       <div>
         <div className="top-bar" id="main-nav">
           <div className="top-bar-left">
-            <ul className="dropdown menu">
-              <li>
+            <ul className="menu">
+              <li className="menu-text">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/B-Flat.svg" width="50px"/>
               </li>
               <li className="menu-text">
                 Binterest
               </li>
-              <li>
+              <li className="menu-text">
                 <div id="search-box">
                   <input type="text" placeholder="Find Stuff"/>
                 </div>
+              </li>
+              <li className="menu-text">
                 <div id="search-button">
-                  <a href="#" class="alert button expand">Search</a>
+                  <a href="#" className="button expand">Search</a>
                 </div>
               </li>
             </ul>
           </div>
           <div className="top-bar-right">
-           <ul className="dropdown menu">
+           <ul className="menu">
             <li className="menu-text">
               <IndexLink to="/#" activeClassName="active-link">Home</IndexLink>
             </li>
             <li className="menu-text">
               <Link to="/#" activeClassName="active-link">Explore</Link>
             </li>
-            {/* <li className="menu-text">
-              <img className="avatar" src="http://tu.dd321.com/dianyingpu/tuji/ZhiQingChun2JiaoYuanRePaiXueShengChuZuYa202609.jpg"/>
-              <Link to="/#" activeClassName="active-link">Crystal</Link>
-            </li> */}
            </ul>
           </div>
         </div>
